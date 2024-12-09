d9d25d0e56548fe6c313e5d8c969b4c20f270a36
Duy Khoa Lê <leduykhoa3@gmail.com>
2024-12-09 22:18:47 +0700

    hi

---
+++ AgriculturalSupplyStore/Controllers/KhachHangController.cs
@@ -0,0 +1,23 @@
+﻿using AgriculturalSupplyStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturalSupplyStore.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private readonly AgriculturalsupplystoreDbContext db;
+
+        public KhachHangController(AgriculturalsupplystoreDbContext context) 
+        {
+            db = context;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
